29a5d6d84515f0fa50bb09e8495b56c6186e65f3
Nishad <nsakkarnaik@gmail>
2024-03-28 11:03:56 +0530

    object repo committed

---
+++ src/main/java/objectRepo/LoginPage.java
@@ -0,0 +1,12 @@
+package objectRepo;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("enter valid credentials and click on login button");
+	}
+
+}
